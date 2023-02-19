83f00babd15140e2bd6e6b9dc482f71cf9b1fe51
AndyHu8 <huandy01@gmail.com>
2023-02-20 00:16:06 +0100

    Startseite fertig

--- vorurteile/src/App.jsx
+++ vorurteile/src/App.jsx
@@ -1,9 +1,11 @@
 import Navigation from "./Navigation/Navigation";
+import Startseite from "./Startseite/Startseite";
 
 function App() {
   return (
     <div>
       <Navigation/>
+      <Startseite/>
     </div>
   );
 }
---
+++ vorurteile/src/Startseite/Startseite.jsx
@@ -0,0 +1,18 @@
+import './Startseite.css';
+import { BsCaretDownFill } from "react-icons/bs";
+import BrokoleeLogo from "../Bilder/brocolee_logo.png";
+
+const Startseite = () => {
+    return (
+        <div id='Startseite' className='startseiteContainer'>
+            <div className='startseiteContent'>
+                <img src={BrokoleeLogo} alt="Brokolee Games Logo" />
+                <h1>Brokolee Games</h1>
+                <p>maybe broccoli doesn't like you either.</p>
+                <BsCaretDownFill size={40} className="ArrowIcon"/>
+            </div>
+        </div>
+    )
+}
+
+export default Startseite;
