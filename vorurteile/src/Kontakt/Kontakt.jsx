0c5cd032aad4ccd001a5e1adeccabaa9b5c389de
AndyHu8 <huandy01@gmail.com>
2023-02-24 23:04:24 +0100

    Kontakt Text changed

--- vorurteile/src/Kontakt/Kontakt.jsx
+++ vorurteile/src/Kontakt/Kontakt.jsx
@@ -5,10 +5,10 @@ const Kontakt = () => {
         <div id="Kontakt" className="kontaktContainer">
             <div className="kontaktContent">
                 <h1>Kontakt / Über uns</h1>
-                <p>Wir sind leidenschaftliche Absolventen der Universität Bayreuth im Bereich der Medienwissenschaften und Spielentwicklung.
-                    Brokolee Games ermöglicht uns unsere Träume des Designs von humorvollen und unterhaltsamen Spielen,
-                    die aber auch zum Nachdenken bringen, zu verwirklichen. Wir sind nicht immer korrekt und brav, dafür aber direkt, ehrlich und herzhaft.
-                    Wir freuen uns auf Sie auf den nächsten Messen, Stammtischen, Gaming-Events, Brettspieleabenden und auch überall anders.</p>
+                <p>Im Fokus von BrokoLee Games liegt immer an erster Stelle der Spielspaß und an zweiter die Gesellschaftskritik.
+                    Wir verbinden innovative Game-Ideen mit Satire und Humor und haben keine Angst davor an gesellschaftliche Grenzen zu gehen,
+                    politisch zu werden und ein klares Statement zu setzen. Unsere persönliche Herausforderung ist es, dass unsere Spiele nicht nur unterhalten,
+                    sondern auch zur kritischen Auseinandersetzung mit globalen Themen unserer Zeit ermutigen.</p>
                 <h2>Derlin & Schödel BrokoLee Games GbR</h2>
                 <h2>Postfach 11 33</h2>
                 <h2>82179 Gröbenzell Deutschland</h2>
