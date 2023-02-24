5802ee000ffe7390a5744ed4cfde845c8a8589ca
AndyHu8 <huandy01@gmail.com>
2023-02-24 22:36:55 +0100

    Vorbereitung für Vorurteile

--- vorurteile/src/App.jsx
+++ vorurteile/src/App.jsx
@@ -3,12 +3,14 @@ import Kontakt from "./Kontakt/Kontakt";
 import Navigation from "./Navigation/Navigation";
 import Newsletter from "./Newsletter/Newsletter";
 import Startseite from "./Startseite/Startseite";
+import Vorurteile from "./Vorurteile/Vorurteile";
 
 function App() {
   return (
     <div>
       <Navigation/>
       <Startseite/>
+      <Vorurteile/>
       <Newsletter/>
       <Kontakt/>
       <Footer/>
