ad520ee183a789977bec999ef6aa3e511f68f89f
AndyHu8 <huandy01@gmail.com>
2023-02-24 23:10:38 +0100

    Kleine fixes für News

--- vorurteile/src/Newsletter/Newsletter.jsx
+++ vorurteile/src/Newsletter/Newsletter.jsx
@@ -8,10 +8,8 @@ const Newsletter = () => {
                 <p>Fühlen Sie sich in unserer Gesellschaft verurteilt? <br/>
                 Im Nachteil? <br/>
                 Von <span style={{ fontWeight: 'bold' }}>Vorurteilen</span> betroffen? <br/>
-                Wir wissen wie Sie sich fühlen & bereiten ein Spiel für Sie vor. <br/>
-                Für mehr Informationen, tragen Sie sich in den Newsletter ein. <br/>
-                <br/>
-                Es wird krass, Sie Schlawiner! ( ͡° ͜ʖ ͡°)</p>
+                Wir wissen wie du dich fühlst & entwickeln ein Spiel für dich. <br/>
+                Für mehr Informationen, trag dich in unseren Newsletter ein. <br/></p>
                 <div className="newsletterInput">
                     <form action="https://formsubmit.co/info@brokolee.games" method="POST">
                         <input name="email" type="email" placeholder="Deine Email Adresse, bitte." required/>
