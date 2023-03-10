5efd407c90bef161f90a0aaa7156a607cd8e2862
AndyHu8 <huandy01@gmail.com>
2023-03-10 19:55:48 +0100

    Startseite changes

--- vorurteile/src/Startseite/Startseite.jsx
+++ vorurteile/src/Startseite/Startseite.jsx
@@ -7,7 +7,6 @@ const Startseite = () => {
         <div id='Startseite' className='startseiteContainer'>
             <div className='startseiteContent'>
                 <img src={headline} alt="Vor[ur]teile" className="headlineImg"/>
-                <h1>Vor[ur]teile Games</h1>
                 <p>Jedes Vorurteil kann auch ein Vorteil sein!</p>
                 <a href='#Vorurteile'><BsCaretDownFill size={50} className="ArrowIcon"/></a>
             </div>
